06707b41cb724a1eccc6129f815348bea9331972
Dennis Ideler <dennis.ideler@moderntreasury.com>
2025-04-29 00:54:30 +0100

    Add error handling to get_connection

--- project/src/db/connection.py
+++ project/src/db/connection.py
@@ -1,11 +1,17 @@
 import os
 import psycopg2
 
+
 def get_connection():
-  return psycopg2.connect(
-    dbname=os.environ["POSTGRES_DB"],
-    user=os.environ["POSTGRES_USER"],
-    password=os.environ["POSTGRES_PASS"],
-    host=os.environ["POSTGRES_HOST"],
-    port=os.environ["POSTGRES_PORT"],
-  )
+    try:
+        return psycopg2.connect(
+            dbname=os.environ["POSTGRES_DB"],
+            user=os.environ["POSTGRES_USER"],
+            password=os.environ["POSTGRES_PASS"],
+            host=os.environ["POSTGRES_HOST"],
+            port=os.environ["POSTGRES_PORT"],
+        )
+    except KeyError as e:
+        raise KeyError(f"Missing required environment variable: {e}")
+    except psycopg2.Error as e:
+        raise Exception(f"Failed to connect to the database: {e}")
